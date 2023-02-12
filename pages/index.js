e3c4e8af338322c478fa461d684ce36db1b087c0
mahmudul165 <mahmudulh883@gmail.com>
2023-02-12 20:28:49 +0600

    header without

--- pages/index.js
+++ pages/index.js
@@ -27,8 +27,8 @@ export default function Home() {
                 <ProductCarousel />
                 <Banner />
                 <CardGallery/>
-                <Outlet />
-                <OurOffices />         
+                {/* <Outlet />
+                <OurOffices />          */}
  
       </main>
     </>
