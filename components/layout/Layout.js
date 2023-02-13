83e94658f0c2c8152d6c80c4d72fa1b489010438
mahmudul165 <mahmudulh883@gmail.com>
2023-02-13 14:10:05 +0600

    added contact us below offices

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import React from "react";
 import Footer from "./Footer";
- import Header from "./Header";
+  import Header from "./Header";
 
  
  
--- pages/contact.js
+++ pages/contact.js
@@ -1,10 +1,12 @@
  
 import React from 'react' 
 import ContactUs from '@/components/contact/ContactUs'
+import OurOffices from '@/components/home/our-offices/OurOffices'
 function contact() {
   return (
     <>
       <ContactUs/>
+      <OurOffices/>
       </>
   )
 }
