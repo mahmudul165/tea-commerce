7f90993c902ab9bfbb133b3c01becba1e7a8d212
mahmudul165 <mahmudulh883@gmail.com>
2023-02-15 02:30:52 +0600

    added gallery style skeleton

--- components/about/Achivements.js
+++ components/about/Achivements.js
@@ -1,38 +1,75 @@
+// import React from 'react';
+// import { Card, Row, Col, Image } from 'react-bootstrap';
+ 
+// const Achivements = ( ) => {
+// //   const achievement = props.achievement;
+// const achievement = {
+//     backgroundImage: '/public/New folder/achievement.jpg',
+//     satisfiedClients: '100+',
+//     expertTeam: '50+',
+//     activeProduct: '20+',
+//     awardWining: '10+',
+//   };
+//   return (
+//     <div >
+//         {/* style={{ backgroundImage: `url(${achievement.backgroundImage})`, backgroundSize: 'cover', height: '50vh' }} */}
+//          <Row className="  align-items-center  " style={{ backgroundImage: `url(https://i.ibb.co/vVN9rcm/achievement.jpg)`, backgroundSize: 'cover', height: '50vh' }}>
+//       <Col xs={12} sm={3} md={3} className="text-center">
+//         <h2 className="text-light fs-2 fw-bolder">{achievement.satisfiedClients}</h2>
+//         <p className="text-light fs-4 fw-bold">Satisfied Clients</p>
+//       </Col>
+//       <Col xs={12} sm={3} md={3} className="text-center">
+//         <h3 className="text-light fs-2 fw-bolder">{achievement.expertTeam}</h3>
+//         <p className="text-light  fs-4 fw-bold">Expert Team</p>
+//       </Col>
+//       <Col xs={12} sm={3} md={3} className="text-center">
+//         <h3 className="text-light fs-2 fw-bolder">{achievement.activeProduct}</h3>
+//         <p className="text-light  fs-4 fw-bold">Active Product</p>
+//       </Col>
+//       <Col xs={12} sm={3} md={3} className="text-center">
+//         <h3 className="text-light fs-2 fw-bolder">{achievement.awardWining}</h3>
+//         <p className="text-light  fs-4 fw-bold">Award Wining</p>
+//       </Col>
+//     </Row>
+//     </div>
+//   );
+// };
+
+// export default Achivements;
+
+
 import React from 'react';
 import { Card, Row, Col, Image } from 'react-bootstrap';
- 
-const Achivements = ( ) => {
-//   const achievement = props.achievement;
-const achievement = {
+
+const Achievements = () => {
+  const achievement = {
     backgroundImage: '/public/New folder/achievement.jpg',
     satisfiedClients: '100+',
     expertTeam: '50+',
     activeProduct: '20+',
     awardWining: '10+',
   };
+
   return (
-    <div >
-        {/* style={{ backgroundImage: `url(${achievement.backgroundImage})`, backgroundSize: 'cover', height: '50vh' }} */}
-         <Row className="  align-items-center  " style={{ backgroundImage: `url(https://i.ibb.co/vVN9rcm/achievement.jpg)`, backgroundSize: 'cover', height: '50vh' }}>
-      <Col xs={12} sm={3} md={3} className="text-center">
+    <Row className="align-items-center p-5" style={{ backgroundImage: `url(https://i.ibb.co/vVN9rcm/achievement.jpg)`, backgroundSize: 'cover', height: 'auto',opacity:'1' }}>
+      <Col xs={12} sm={6} md={3} className="text-center mb-4">
         <h2 className="text-light fs-2 fw-bolder">{achievement.satisfiedClients}</h2>
         <p className="text-light fs-4 fw-bold">Satisfied Clients</p>
       </Col>
-      <Col xs={12} sm={3} md={3} className="text-center">
+      <Col xs={12} sm={6} md={3} className="text-center mb-4">
         <h3 className="text-light fs-2 fw-bolder">{achievement.expertTeam}</h3>
-        <p className="text-light  fs-4 fw-bold">Expert Team</p>
+        <p className="text-light fs-4 fw-bold">Expert Team</p>
       </Col>
-      <Col xs={12} sm={3} md={3} className="text-center">
+      <Col xs={12} sm={6} md={3} className="text-center mb-4">
         <h3 className="text-light fs-2 fw-bolder">{achievement.activeProduct}</h3>
-        <p className="text-light  fs-4 fw-bold">Active Product</p>
+        <p className="text-light fs-4 fw-bold">Active Product</p>
       </Col>
-      <Col xs={12} sm={3} md={3} className="text-center">
+      <Col xs={12} sm={6} md={3} className="text-center mb-4">
         <h3 className="text-light fs-2 fw-bolder">{achievement.awardWining}</h3>
-        <p className="text-light  fs-4 fw-bold">Award Wining</p>
+        <p className="text-light fs-4 fw-bold">Award Winning</p>
       </Col>
     </Row>
-    </div>
   );
 };
 
-export default Achivements;
+export default Achievements;
--- components/about/ClientCompany.js
+++ components/about/ClientCompany.js
@@ -25,25 +25,25 @@ export default class ClientCompany extends Component {
       cssEase: "linear",
     };
     return (
-      <div className="container">
-        <h1 className="text-center fs-3 fw-bolder my-5 ">
+      <div className="container my-3">
+        <h1 className="text-center fs-3 fw-bolder my-3 py-3">
           Trusted by 1,00+ Companies Worldwide
         </h1>
-        <div className="container my-4 py-4">
+        <div className="container my-2 py-3">
           <div className="container text-center my-4 py-4">
             <Slider {...settings}>
-              <section className="p-2  ">
+              <section className="p-1  ">
                 <Image src={CompanyOne} alt="" className="w-75 h-75" />
               </section>
-              <div className="p-2 ">
+              <div className="p-1 ">
                 {/* <img src="/favicon.ico" alt="" className="w-25 h-25" /> */}
-                <Image src={CompanyTwo} alt="" className="w-25 h-25" />
+                <Image src={CompanyTwo} alt="" className="w-75 h-25" />
               </div>
-              <div className="p-2">
-                {/* <img src="/vercel.svg" alt="" className="w-50 h-50" /> */}
-                <Image src={CompanyThree} alt="" className="w-50 h-50" />
+              <div className="p-1">
+                {/* <img src="/vercel.svg" alt="" className="w-75 h-50" /> */}
+                <Image src={CompanyThree} alt="" className="w-75 h-50" />
               </div>
-              <div className="p-2">
+              <div className="p-1">
                 {/* <img
                   src="/images/home/logo_influencer.png"
                   alt=""
@@ -51,23 +51,23 @@ export default class ClientCompany extends Component {
                 /> */}
                 <Image src={CompanyOne} alt="" className="w-75 h-75" />
               </div>
-              <div className="p-2">
+              <div className="p-1">
                 {/* <img src="/favicon.ico" alt="" className="w-25 h-25" /> */}
-                <Image src={CompanyTwo} alt="" className="w-25 h-25" />
+                <Image src={CompanyTwo} alt="" className="w-75 h-25" />
               </div>
-              <div className="p-2">
-                {/* <img src="/vercel.svg" alt="" className="w-50 h-50" /> */}
-                <Image src={CompanyThree} alt="" className="w-50 h-50" />
+              <div className="p-1">
+                {/* <img src="/vercel.svg" alt="" className="w-75 h-50" /> */}
+                <Image src={CompanyThree} alt="" className="w-75 h-50" />
               </div>
-              <div className="p-2">
+              <div className="p-1">
                 {/* <img src="/favicon.ico" alt="" className="w-25 h-25" /> */}
-                <Image src={CompanyTwo} alt="" className="w-25 h-25" />
+                <Image src={CompanyTwo} alt="" className="w-75 h-25" />
               </div>
-              <div className="p-2">
-                {/* <img src="/vercel.svg" alt="" className="w-50 h-50" /> */}
-                <Image src={CompanyThree} alt="" className="w-50 h-50" />
+              <div className="p-1">
+                {/* <img src="/vercel.svg" alt="" className="w-75 h-50" /> */}
+                <Image src={CompanyThree} alt="" className="w-75 h-50" />
               </div>
-              <div className="p-2">
+              <div className="p-1">
                 {/* <img
                   src="/images/home/logo_influencer.png"
                   alt=""
@@ -75,13 +75,13 @@ export default class ClientCompany extends Component {
                 /> */}
                 <Image src={CompanyOne} alt="" className="w-75 h-75" />
               </div>
-              <div className="p-2">
+              <div className="p-1">
                 {/* <img src="/favicon.ico" alt="" className="w-25 h-25" /> */}
-                <Image src={CompanyTwo} alt="" className="w-25 h-25" />
+                <Image src={CompanyTwo} alt="" className="w-75 h-25" />
               </div>
-              <div className="p-2 ">
-                {/* <img src="/vercel.svg" alt="" className="w-50 h-50" /> */}
-                <Image src={CompanyThree} alt="" className="w-50 h-50" />
+              <div className="p-1 ">
+                {/* <img src="/vercel.svg" alt="" className="w-75 h-50" /> */}
+                <Image src={CompanyThree} alt="" className="w-75 h-50" />
               </div>
             </Slider>
           </div>
