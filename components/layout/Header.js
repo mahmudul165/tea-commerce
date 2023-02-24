4331782da037ac8483f82c01c3cce63332aac558
gazirahad7 <65822873+gazirahad7@users.noreply.github.com>
2023-02-24 23:32:14 +0600

    Add: responsive mobile menu for xs, sm devices

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -109,15 +109,11 @@
 import Image from "next/image";
 import Link from "next/link";
 import { useEffect, useState } from "react";
-import { Col, Row } from "react-bootstrap";
-import Button from "react-bootstrap/Button";
-import Container from "react-bootstrap/Container";
-import Form from "react-bootstrap/Form";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import Offcanvas from "react-bootstrap/Offcanvas";
-import { FaShoppingCart, FaSearch } from "react-icons/fa";
+import { FaSearch, FaShoppingCart } from "react-icons/fa";
 import { useCart } from "react-use-cart";
 import Logo from "/public/logo.png";
 function Header() {
@@ -151,139 +147,146 @@ function Header() {
     <>
       {["lg"].map((expand) => (
         <Navbar
-               
           key={expand}
-           sticky="top"  
+          sticky="top"
           // bg="light"
           expand={expand}
           // className="  fs-6 fw-bold text-danger  navPosition"
-          className={`row align-items-center   m-auto fs-6 fw-bold text-danger   navPosition  ${scroll ? " bg-light container-fluid " : "container"}`}
+          className={`row align-items-center   m-auto fs-6 fw-bold text-danger   navPosition  ${
+            scroll ? " bg-light container-fluid " : "container"
+          }`}
         >
           {/* <Container fluid className="justify-content-around"> */}
-            {/* <div className="row"> */}
-              <div className="col-md-4">
-                <Nav className="mr-auto " as="ul">
-                  <Nav.Item as="li">                    
-                    <Nav.Link as="span">
-                      <Link href={"/"}>Home</Link>
-                    </Nav.Link>
-                  </Nav.Item>
-                  {/* <Nav.Item as="li">                    
+          {/* <div className="row"> */}
+          <div className="col-md-4    d-none d-md-block  ">
+            <Nav className="mr-auto " as="ul">
+              <Nav.Item as="li">
+                <Nav.Link as="span">
+                  <Link href={"/"}>Home</Link>
+                </Nav.Link>
+              </Nav.Item>
+              {/* <Nav.Item as="li">                    
                     <Nav.Link as="span" className="text-nowrap">
                       <Link href={"/about"}>About Us</Link>
                     </Nav.Link>
                   </Nav.Item> */}
-                   
-                  <NavDropdown title="About Us" id="collasible-nav-dropdown">
-                    <NavDropdown.Item as="span">
-                       <Link href={"/about"}>About Sultan Tea</Link>
-                    </NavDropdown.Item>                     
-                    {/* <NavDropdown.Divider /> */}
-                    <NavDropdown.Item as="span">
-                       <Link href={"/new-arrival"}>Press Releases</Link>
-                    </NavDropdown.Item>  
-                    <NavDropdown.Item as="span">
-                       <Link href={"/new-arrival"}>News Links</Link>
-                    </NavDropdown.Item>                
-                  </NavDropdown>
 
-                  <NavDropdown title="Our Business" id="collasible-nav-dropdown">
-                    <NavDropdown.Item as="span">
-                       <Link href={"/about"}>New</Link>
-                    </NavDropdown.Item>                     
-                    {/* <NavDropdown.Divider /> */}
-                    {/* <NavDropdown.Item as="span">
+              <NavDropdown title="About Us" id="collasible-nav-dropdown">
+                <NavDropdown.Item as="span">
+                  <Link href={"/about"}>About Sultan Tea</Link>
+                </NavDropdown.Item>
+                {/* <NavDropdown.Divider /> */}
+                <NavDropdown.Item as="span">
+                  <Link href={"/new-arrival"}>Press Releases</Link>
+                </NavDropdown.Item>
+                <NavDropdown.Item as="span">
+                  <Link href={"/new-arrival"}>News Links</Link>
+                </NavDropdown.Item>
+              </NavDropdown>
+
+              <NavDropdown title="Our Business" id="collasible-nav-dropdown">
+                <NavDropdown.Item as="span">
+                  <Link href={"/about"}>New</Link>
+                </NavDropdown.Item>
+                {/* <NavDropdown.Divider /> */}
+                {/* <NavDropdown.Item as="span">
                        <Link href={"/new-arrival"}>Press Releases</Link>
                     </NavDropdown.Item>   */}
-                                
-                  </NavDropdown>
-                  <Nav.Link as="span" className="text-nowrap">
-                      <Link href={"/our-brand"}>Our Brands</Link>
-                    </Nav.Link>
-                  
-                </Nav>
-              </div>
-              <div className="col-md-2">
-                <Nav
-                  className="m-auto align-items-center  justify-content-center "
-                  as="ul"
-                >
-                  {" "}
-                  <Nav.Item as="li" className="d-flex ">
-                  <Image
-      src={Logo}
-      alt="logo"
-      width={50}
-      height={50}
-      layout={500}
-    />
-                    {/* <img
+              </NavDropdown>
+              <Nav.Link as="span" className="text-nowrap">
+                <Link href={"/our-brand"}>Our Brands</Link>
+              </Nav.Link>
+            </Nav>
+          </div>
+          <div className="col-md-2  col-sm-2  col-2">
+            <Nav
+              className="m-auto align-items-center  justify-content-center "
+              as="ul"
+            >
+              {" "}
+              <Nav.Item as="li" className="d-flex ">
+                <Image
+                  src={Logo}
+                  alt="logo"
+                  width={50}
+                  height={50}
+                  layout={500}
+                />
+                {/* <img
                       src="/public/logo.png"
                       alt="logo"
                       className="d-inline-block mr-3 "
                       // style={{ maxHeight: "80px" }}
                     />{" "} */}
-                  </Nav.Item>
-                </Nav>
-              </div>
-              <div className="col-md-6">
-                <Navbar.Toggle
-                  aria-controls={`offcanvasNavbar-expand-${expand}`}
-                />
-                <Navbar.Offcanvas
-                  id={`offcanvasNavbar-expand-${expand}`}
-                  aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
-                  placement="end"
-                >
-                  <Offcanvas.Header closeButton>
-                    <Offcanvas.Title
-                      id={`offcanvasNavbarLabel-expand-${expand}`}
-                    >
-                      Sultan Tea
-                    </Offcanvas.Title>
-                  </Offcanvas.Header>
-                  <Offcanvas.Body>
-                    <Nav className="justify-content-end flex-grow-1 pe-3">
-                    
-                    <Nav.Link as="span">
-                      <Link href={"/gellary"}>Gallery</Link>
-                    </Nav.Link>
-                    <NavDropdown title="Shop" id="collasible-nav-dropdown">
+              </Nav.Item>
+            </Nav>
+          </div>
+
+          <div className=" d-block d-sm-block d-md-none col-sm-2 offset-6 col-2 d-xs-block ">
+            <Nav.Link as="i" className="fs-4">
+              <Link href={"/add-to-card"}>
+                {" "}
+                <FaShoppingCart />{" "}
+              </Link>
+            </Nav.Link>
+          </div>
+
+          <div className="col-md-6  col-sm-2   col-2">
+            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
+            <Navbar.Offcanvas
+              id={`offcanvasNavbar-expand-${expand}`}
+              aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+              placement="end"
+            >
+              <Offcanvas.Header closeButton>
+                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
+                  Sultan Tea
+                </Offcanvas.Title>
+              </Offcanvas.Header>
+              <Offcanvas.Body>
+                <Nav className="justify-content-end flex-grow-1 pe-3">
+                  <Nav.Link as="span">
+                    <Link href={"/gellary"}>Gallery</Link>
+                  </Nav.Link>
+                  <NavDropdown title="Shop" id="collasible-nav-dropdown">
                     <NavDropdown.Item as="span">
-                       <Link href={"/shop"}>Products</Link>
-                    </NavDropdown.Item>                     
+                      <Link href={"/shop"}>Products</Link>
+                    </NavDropdown.Item>
                     {/* <NavDropdown.Divider /> */}
                     <NavDropdown.Item as="span">
-                       <Link href={"/new-arrival"}>New Arrivals</Link>
+                      <Link href={"/new-arrival"}>New Arrivals</Link>
                     </NavDropdown.Item>
                   </NavDropdown>
-                    <Nav.Link as="span">
-                      <Link href={"/blogs"}>Blog</Link>
-                    </Nav.Link>
-                    <Nav.Link as="span">
-                        <Link href={"/carrier"}>Carrier</Link>
-                      </Nav.Link>
-                      <Nav.Link as="span" className="text-nowrap">
-                        <Link href={"/contact"}>Contact Us</Link>
-                      </Nav.Link>
-                      {/* start */}
-                      <Nav.Link as="i" className="fs-4">
-                        <Link href={"/add-to-card"}>   <FaShoppingCart />  </Link>
-                      </Nav.Link>
-                      
-                      {/* ({totalItems}) */}
-                    
-                 
-                    <Nav.Link as="i" className="fs-4">
-                        <Link href={"/search"}>  <FaSearch /> </Link>
-                      </Nav.Link>
-                       
-                    </Nav>
-                    
-                  </Offcanvas.Body>
-                </Navbar.Offcanvas>
-              </div>
-            {/* </div> */}
+                  <Nav.Link as="span">
+                    <Link href={"/blogs"}>Blog</Link>
+                  </Nav.Link>
+                  <Nav.Link as="span">
+                    <Link href={"/carrier"}>Carrier</Link>
+                  </Nav.Link>
+                  <Nav.Link as="span" className="text-nowrap">
+                    <Link href={"/contact"}>Contact Us</Link>
+                  </Nav.Link>
+                  {/* start */}
+                  <Nav.Link as="i" className="fs-4">
+                    <Link href={"/add-to-card"}>
+                      {" "}
+                      <FaShoppingCart />{" "}
+                    </Link>
+                  </Nav.Link>
+
+                  {/* ({totalItems}) */}
+
+                  <Nav.Link as="i" className="fs-4">
+                    <Link href={"/search"}>
+                      {" "}
+                      <FaSearch />{" "}
+                    </Link>
+                  </Nav.Link>
+                </Nav>
+              </Offcanvas.Body>
+            </Navbar.Offcanvas>
+          </div>
+          {/* </div> */}
           {/* </Container> */}
         </Navbar>
       ))}
