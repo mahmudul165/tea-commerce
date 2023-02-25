ece6219b43cb52535956e728afba4877727374e7
gazirahad7 <65822873+gazirahad7@users.noreply.github.com>
2023-02-25 22:10:49 +0600

    Created: component for location card & contact from

--- pages/contact-updated.js
+++ pages/contact-updated.js
@@ -5,6 +5,76 @@ import OurOffices from "@/components/home/our-offices/OurOffices";
 import { Card, Form } from "react-bootstrap";
 import { FaEnvelope, FaMapMarkerAlt, FaPhoneAlt } from "react-icons/fa";
 
+const OurLocationCard = () => {
+  return (
+    <>
+      <Card className="card border rounded w-25 p-4  position-absolute top-50  mx-5 translate-middle-y d-sm-none d-md-block d-xs-none pb-5">
+        <div className="d-grid my-4">
+          <MyButton
+            style={{ backgroundColor: "#59330E" }}
+            type="button"
+            size="lg"
+            className="fw-bold text-white fs-5"
+          >
+            Our Locations{" "}
+          </MyButton>
+        </div>
+        <h5 className="my-2">Located in Green Road</h5>
+        <div className="mt-2">
+          <p>
+            Address: Corporate Office: Plot No. SW(I) 4, 25 Gulshan Avenue 1212,
+            Bangladesh
+          </p>
+          <p>Tel: +88 02 9888211</p>
+          <p>Fax: 88 02 9888158, 1 Bir Uttam Mir Shawkat Sarak, Dhaka 1208</p>
+        </div>
+      </Card>
+    </>
+  );
+};
+
+const ContactFrom = () => {
+  return (
+    <>
+      <Form
+        id="contact-form"
+        name="contact-form p-2"
+        action="mail.php"
+        method="POST"
+      >
+        <h2 className="mb-2 fs-5 fw-bold">Contact From</h2>
+        <Form.Group controlId="name" className="mb-3">
+          <Form.Control type="text" name="name" placeholder="Name" />
+        </Form.Group>
+
+        <Form.Group controlId="email" className="mb-3">
+          <Form.Control type="text" name="email" placeholder="Email" />
+        </Form.Group>
+        <Form.Group controlId="phone" className="mb-3">
+          <Form.Control type="text" name="phone" placeholder="Phone" />
+        </Form.Group>
+
+        <Form.Group controlId="message" className="mb-3">
+          <Form.Control
+            className="p-4"
+            as="textarea"
+            rows={10}
+            name="message"
+            placeholder="Message"
+          />
+        </Form.Group>
+        <MyButton
+          style={{ backgroundColor: "#E49E48" }}
+          type="submit"
+          size="lg"
+          className=" text-white px-5"
+        >
+          SEND
+        </MyButton>
+      </Form>
+    </>
+  );
+};
 function contact() {
   return (
     <>
@@ -12,29 +82,7 @@ function contact() {
       <RouteNavSlider router="Contact Us" />
       <div className="  container my-5  ">
         <div className="rounded border position-relative">
-          <Card className="card border rounded w-25 p-4  position-absolute top-50 mx-5 translate-middle-y d-sm-none d-md-block d-xs-none pb-5">
-            <div className="d-grid my-4">
-              <MyButton
-                style={{ backgroundColor: "#59330E" }}
-                type="button"
-                size="lg"
-                className="fw-bold text-white fs-5"
-              >
-                Our Locations{" "}
-              </MyButton>
-            </div>
-            <h5 className="my-2">Located in Green Road</h5>
-            <div className="mt-2">
-              <p>
-                Address: Corporate Office: Plot No. SW(I) 4, 25 Gulshan Avenue
-                1212, Bangladesh
-              </p>
-              <p>Tel: +88 02 9888211</p>
-              <p>
-                Fax: 88 02 9888158, 1 Bir Uttam Mir Shawkat Sarak, Dhaka 1208
-              </p>
-            </div>
-          </Card>
+          <OurLocationCard />
           <iframe
             src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=115%20Ahmedbag,%20Global%20Islami%20Bank%20Building,%20Budhho%20Mondir,%20Dhaka-1214%201214%20Dhaka,%20Dhaka%20Division,%20Bangladesh+(My%20Business%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
             width="100%"
@@ -87,42 +135,7 @@ function contact() {
             </Card>
           </div>
           <div className="col-md-8">
-            <Form
-              id="contact-form"
-              name="contact-form p-2"
-              action="mail.php"
-              method="POST"
-            >
-              <h2 className="mb-2 fs-5 fw-bold">Contact From</h2>
-              <Form.Group controlId="name" className="mb-3">
-                <Form.Control type="text" name="name" placeholder="Name" />
-              </Form.Group>
-
-              <Form.Group controlId="email" className="mb-3">
-                <Form.Control type="text" name="email" placeholder="Email" />
-              </Form.Group>
-              <Form.Group controlId="phone" className="mb-3">
-                <Form.Control type="text" name="phone" placeholder="Phone" />
-              </Form.Group>
-
-              <Form.Group controlId="message" className="mb-3">
-                <Form.Control
-                  className="p-4"
-                  as="textarea"
-                  rows={10}
-                  name="message"
-                  placeholder="Message"
-                />
-              </Form.Group>
-              <MyButton
-                style={{ backgroundColor: "#E49E48" }}
-                type="submit"
-                size="lg"
-                className=" text-white px-5"
-              >
-                SEND
-              </MyButton>
-            </Form>
+            <ContactFrom />
           </div>
         </div>
       </div>
