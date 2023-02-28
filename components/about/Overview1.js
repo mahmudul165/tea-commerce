3f3c4ee0a04d6b53bb54fbb375276a0cc88d7257
gazirahad7 <65822873+gazirahad7@users.noreply.github.com>
2023-02-28 09:09:52 +0600

    Update: our chairman section in about us

--- components/about/Overview1.js
+++ components/about/Overview1.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import React from "react";
 import { Card } from "react-bootstrap";
- import OverViewImage2 from '/public/about-image-1.png'
+import OverViewImage2 from "/public/about-image-1.png";
 function Overview1() {
   return (
     <section>
@@ -16,33 +16,38 @@ function Overview1() {
                     transition: { duration: 2 },
                   }} "
               >
-               Our Chairmen
+                Our Chairmen
               </h1>
-              <h6
-                className="my-2 py-2 fs-5 fw-bold  whileHover={{
+              <p
+                className="my-2 py-2 fs-4   whileHover={{
                     scale: 1.3,
                     transition: { duration: 2 },
                   }} "
               >
-             Lorem Ipsum passage, and going through the cites of the word in classical literature.
-              </h6>
-              <p className="py-1 fs-5">
+                Lorem Ipsum passage, and going through the cites of the word in
+                classical literature.
+              </p>
+              <p className="py-1 fs-5 text-justify">
                 St. Augustine College Now Offers A Fully Online English Language
                 Program. Learn more today. Learn English Today! Take An Online
                 ESL Course For Adults Through St. Augustine College. Check
-                Academic Calendar. Browse Resources. View Events.  
+                Academic Calendar. Browse Resources. View Events.
               </p>
             </div>
             {/* </Roll> */}
           </section>
-          <section className=" col-md-6 col-sm-12 ">
+          <section className=" col-md-6 col-sm-12 d-flex justify-content-end ">
             {/* <Roll right> */}
-              <div className="my-4 py-2">
-                <Card> 
-                <Image className="heroImage" src={OverViewImage2} alt="Picture of the author" />
-                </Card>
-              </div>
-          
+            <div className="my-4 py-2 ">
+              <Card className="border-0">
+                <Image
+                  className="heroImage"
+                  src={OverViewImage2}
+                  alt="Picture of the author"
+                  width={495}
+                />
+              </Card>
+            </div>
           </section>
         </div>
       </section>
