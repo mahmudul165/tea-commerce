5c40d44c45db2a3566326c1c34b9c5450691082c
mahmudul165 <mahmudulh883@gmail.com>
2023-03-01 22:30:08 +0600

    integation add to cart

--- components/common/CartItemsModal.js
+++ components/common/CartItemsModal.js
@@ -15,50 +15,81 @@ import {
 } from "react-icons/ai";
 import { FaShoppingCart } from "react-icons/fa";
 import { RiDeleteBin5Fill } from "react-icons/ri";
+import { useCart } from "react-use-cart";
+
 const CartItem = ({ item }) => {
+  const {
+    isEmpty,
+    totalUniqueItems,
+    items,
+    totalItems,
+    cartTotal,
+    updateItemQuantity,
+    removeItem,
+    emptyCart,
+  } = useCart();
+  console.log(items);
   return (
     <>
       <div className="">
         <div className="row ele-center ">
-          <div className="col-3">
-            <div className="border p-2 rounded ele-center w-75">
-              <img
-                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS8uxkKiX-wGly90GY9jHmIUew80yVpI6jzAg&usqp=CAU"
-                alt="Product"
-                height={100}
-                width={100}
-              />
-            </div>
-          </div>
-          <div className="col-8">
-            <div>
-              <p className="fs-6 fw-bold cus-color-secondary my-3">
-                Sultan Tea CD 500gm
-              </p>
-              <div className="d-flex gap-4">
-                <p className="fs-4 p-2">TK 255</p>
-
-                <div className="p-1 border  rounded">
-                  <button
-                    className="btn btn-sm  fw-bold border-end"
-                    type="button"
-                  >
-                    <AiOutlinePlus size={22} />
-                  </button>
-                  <span className="fw-bold fs-6 p-2">1</span>
-                  <button className="btn border-start btn-sm" type="button">
-                    <AiOutlineMinus size={22} />
-                  </button>
+          {items.map((item, index) => (
+            <>
+              <div className="col-3">
+                <div className="border p-2 rounded ele-center w-75">
+                  <img
+                    src={item?.images[0].url}
+                    alt="Product"
+                    height={100}
+                    width={100}
+                  />
                 </div>
               </div>
-            </div>
-          </div>
-          <div className="col-1 ele-center">
-            {" "}
-            <IconWithBackground>
-              <RiDeleteBin5Fill size={22} className="" />
-            </IconWithBackground>
-          </div>
+              <div className="col-8">
+                <div>
+                  <p className="fs-6 fw-bold cus-color-secondary my-3">
+                    {/* Sultan Tea CD 500gm */}
+                    {item.name}
+                  </p>
+                  <div className="d-flex gap-4">
+                    <p className="fs-4 p-2">TK {item.price}</p>
+
+                    <div className="p-1 border  rounded">
+                      <button
+                        className="btn btn-sm  fw-bold border-end"
+                        type="button"
+                      >
+                        <AiOutlinePlus size={22} 
+                         onClick={() =>
+                          updateItemQuantity(
+                            item.id,
+                            item.quantity + 1
+                          )
+                        }
+                        />
+                      </button>
+                      <span className="fw-bold fs-6 p-2">{item.quantity}</span>
+                      <button className="btn border-start btn-sm" type="button">
+                        <AiOutlineMinus size={22} onClick={() =>
+                                      updateItemQuantity(
+                                        item.id,
+                                        item.quantity - 1
+                                      )
+                                    } />
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-1 ele-center">
+                <button onClick={() => removeItem(item.id)}> 
+                <IconWithBackground    >
+                  <RiDeleteBin5Fill size={22} className=""   />
+                </IconWithBackground>
+                </button>
+              </div>
+            </>
+          ))}
         </div>
       </div>
       <hr className="my-4"></hr>
@@ -104,6 +135,17 @@ const CheckOutForm = () => {
   );
 };
 const CartItemsModal = (props) => {
+  const {
+    isEmpty,
+    totalUniqueItems,
+    items,
+    totalItems,
+    cartTotal,
+    updateItemQuantity,
+    removeItem,
+    emptyCart,
+  } = useCart();
+
   return (
     <Modal
       {...props}
@@ -115,7 +157,7 @@ const CartItemsModal = (props) => {
         <Modal.Title id="contained-modal-title-vcenter">
           <div className="d-flex align-items-center gap-3">
             <AiOutlineShoppingCart size={40} />
-            <p className="fs-5 fw-bold">2 items</p>
+            <p className="fs-5 fw-bold">{totalItems} items</p>
           </div>
         </Modal.Title>
       </Modal.Header>
@@ -124,14 +166,14 @@ const CartItemsModal = (props) => {
           <div className="row ">
             <div className="col-12  ">
               <CartItem />
-              <CartItem />
+              {/* <CartItem /> */}
 
               <div className="d-flex justify-content-between my-4 ">
                 <div>
                   <p className="fs-4 fw-bold">Total</p>
                 </div>
                 <div>
-                  <p className="fs-4 fw-bold">Tk 645,00</p>
+                  <p className="fs-4 fw-bold">Tk {cartTotal}</p>
                 </div>
               </div>
               <hr className="my-4  d-md-none "></hr>
