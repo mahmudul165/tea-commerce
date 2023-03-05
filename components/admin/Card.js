d82b0ba501519ebb9ca4191c395f9c663fc5b37a
mahmudul165 <mahmudulh883@gmail.com>
2023-03-05 14:31:44 +0600

    added admin dashboard auth context login pge added to layout and adminlayout

---
+++ components/admin/Card.js
@@ -0,0 +1,45 @@
+import { MdCallMade } from "react-icons/md";
+
+function Card({ name, number, bgColor, path }) {
+  console.log("first bg", bgColor);
+  return (
+    <>
+   
+    
+      {bgColor ? (
+        <a
+          href={`${path ? path : "/dashboard"}`}
+          className="text-decoration-none"
+        >
+          <div
+            className={`card border-0 shadow-sm rounded-lg ${bgColor}`}
+            style={{ height: "100%" }}
+          >
+            <div className="card-body d-flex flex-column justify-content-between">
+              <div className="d-flex justify-content-end">
+              <MdCallMade className="fs-1 fw-bolder bg-white  p-2    rounded-circle " />{" "}
+              </div>
+              <h3 className="card-title text-white fs-4 fw-bold text-center">
+                {number ? number : 0}
+              </h3>
+              <p className="card-text text-white fs-6 fw-bold text-center">
+                {name}
+              </p>
+             
+
+               
+            </div>
+            <div className="text-center"> <hr className="divider fs-1 fw-bolder p-0 mt-0" />
+             <span className="badge text-center">{name}</span>
+             </div>
+          </div>
+        </a>
+      ) : (
+        <>empty</>
+      )}
+       
+    </>
+  );
+}
+
+export default Card;
--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,14 +1,93 @@
+import Card from "@/components/admin/Card";
 import PrivateRoute from "@/components/PrivateRoute";
 
 const DashboardPage = () => {
   return (
     <PrivateRoute>
-      <div>
-        <h1>Dashboard</h1>
-        <p>
-          This is the dashboard page. Only logged-in users can access it.
-        </p>{" "}
-      </div>
+      
+  <main className="p-6  space-y-6 my-1">
+  <section className="row row-cols-1 row-cols-md-2 row-cols-xl-4 g-6 gy-2 p-3" > 
+   
+      
+        <Card
+          name="Total Order"
+          path="/patient/create-new-patient"
+          number="300"
+          bgColor="bg-warning "
+          // style={{colo:'#dc2626'}}
+        />
+        <Card
+          name="Total Customer"
+          path="/all-ticket/create-new-ticket"
+          number="73"
+          bgColor="bg-dark"
+        />
+        <Card
+          name="Sell"
+          path="/therapist/create-new-therapist"
+          number="37"
+          bgColor="bg-danger"
+        />
+        <Card
+          name="New Products"
+          path="/appointment"
+          number="455"
+          bgColor="bg-secondary"
+        />
+         <Card
+          name="Sell"
+          path="/therapist/create-new-therapist"
+          number="37"
+          bgColor="bg-danger"
+        />
+        <Card
+          name="New Products"
+          path="/appointment"
+          number="455"
+          bgColor="bg-primary"
+        />
+        <Card
+          name="Total Order"
+          path="/patient/create-new-patient"
+          number="300"
+          bgColor="bg-warning "
+          // style={{colo:'#dc2626'}}
+        />
+        <Card
+          name="Total Customer"
+          path="/all-ticket/create-new-ticket"
+          number="73"
+          bgColor="bg-dark"
+        />
+         <Card
+          name="Sell"
+          path="/therapist/create-new-therapist"
+          number="37"
+          bgColor="bg-primary"
+        />
+        <Card
+          name="New Products"
+          path="/appointment"
+          number="455"
+          bgColor="bg-secondary"
+        />
+         <Card
+          name="Sell"
+          path="/therapist/create-new-therapist"
+          number="37"
+          bgColor="bg-danger"
+        />
+         <Card
+          name="Total Order"
+          path="/patient/create-new-patient"
+          number="300"
+          bgColor="bg-warning "
+          // style={{colo:'#dc2626'}}
+        />
+       
+      </section>
+   </main>
+   
     </PrivateRoute>
   );
 };
