8739c8d9235d4cc7412c278a54fedaad9c0942ed
gazirahad7 <65822873+gazirahad7@users.noreply.github.com>
2023-03-06 11:48:58 +0600

    Update: layout

--- components/layoutAdmin/Layoutt.js
+++ components/layoutAdmin/Layoutt.js
@@ -1,5 +1,5 @@
-import Header from './Header';
-import Sidebar from './Sidebar';
+import Header from "./Header";
+import Sidebar from "./Sidebar";
 
 const Layout = ({ children }) => {
   return (
@@ -7,7 +7,9 @@ const Layout = ({ children }) => {
       <Header />
       <div className="row">
         <Sidebar />
-        <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">{children}</main>
+        <main className="col-md-9  ms-sm-auto col-lg-10 px-md-4">
+          {children}
+        </main>
       </div>
     </div>
   );
