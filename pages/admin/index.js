76d78826f6700f9c9ea3489c5733a5f14cf0cb1e
gazirahad7 <65822873+gazirahad7@users.noreply.github.com>
2023-03-06 11:54:13 +0600

    Update: login from

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,16 +1,15 @@
-import { useState } from 'react';
-import { useRouter } from 'next/router';
-import axios from 'axios'; 
-import useAuth from '@/lib/hook/useAuth';
+import useAuth from "@/lib/hook/useAuth";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import { FaUser } from "react-icons/fa";
 function LoginPage() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
   const router = useRouter();
-  const { apiRootUrl,    apiEndpoint  } = useAuth();
-  
- console.log('first',  apiRootUrl,
- apiEndpoint );
+  const { apiRootUrl, apiEndpoint } = useAuth();
+
+  console.log("first", apiRootUrl, apiEndpoint);
   const handleSubmit = async (event) => {
     event.preventDefault();
     // `${apiRootUrl}${apiEndpoint?.ticket?.list}`
@@ -25,25 +24,25 @@ function LoginPage() {
     //       'Access-Control-Allow-Origin': '*'
     //     }
     //   });
-  
+
     //   if (response.status === 200) {
     //     const user = await response;
     //     // export function saveUserSession(user) {
     //     //     sessionStorage.setItem("user", JSON.stringify(user));
     //     //   }
-          
+
     //     //   export function getUserSession() {
     //     //     const user = sessionStorage.getItem("user");
     //     //     return user ? JSON.parse(user) : null;
     //     //   }
-          
+
     //     //   export function clearUserSession() {
     //     //     sessionStorage.removeItem("user");
     //     //   }
     //     // If login is successful, set session data in session storage
     //     sessionStorage.setItem("user", JSON.stringify(user));
     //      sessionStorage.setItem('isLoggedIn', true);
-  
+
     //     // Redirect to the dashboard page
     //     router.push('/admin/dashboard');
     //   } else {
@@ -55,26 +54,55 @@ function LoginPage() {
     //   setError('Login failed');
     // }
   };
-  
+
   return (
-    <div className="container">
-      <h1 className="text-center my-5">Login</h1>
-      {error && <div className="alert alert-danger">{error}</div>}
-      <form onSubmit={handleSubmit}>
-        <div className="form-floating mb-3">
-          <input type="text" className="form-control" id="username" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
-          <label htmlFor="username">Username</label>
+    <div className="container    my-5 ">
+      <div className="row">
+        <div className="col-md-6 offset-md-3 border p-5  rounded-3 shadow-sm  ">
+          <div className="text-center">
+            {" "}
+            <FaUser size={60} className="cus-color-primary" />
+          </div>
+          <h4 className="text-center mt-4">Welcome :)</h4>
+
+          <h1 className="text-center my-5">Admin Login</h1>
+          {error && <div className="alert alert-danger">{error}</div>}
+          <form onSubmit={handleSubmit}>
+            <div className="form-floating mb-3">
+              <input
+                type="text"
+                className="form-control"
+                id="username"
+                placeholder="Username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+              <label htmlFor="username">Username</label>
+            </div>
+            <div className="form-floating mb-3">
+              <input
+                type="password"
+                className="form-control"
+                id="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <label htmlFor="password">Password</label>
+            </div>
+            <button
+              type="submit"
+              className="btn btn-lg  w-100 cus-bg-primary px-4 text-white  fs-5 fw-bold"
+            >
+              Login
+            </button>
+          </form>
         </div>
-        <div className="form-floating mb-3">
-          <input type="password" className="form-control" id="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
-          <label htmlFor="password">Password</label>
-        </div>
-        <button type="submit" className="btn btn-primary">Login</button>
-      </form>
+      </div>
     </div>
   );
 }
-export default  LoginPage;
+export default LoginPage;
 LoginPage.getLayout = function PageLayout(page) {
-    return <>{page}</>;
-  };
+  return <>{page}</>;
+};
