8a367133540c564297466f11b03afddfccf94994
gazirahad7 <65822873+gazirahad7@users.noreply.github.com>
2023-03-07 13:44:47 +0600

    Add: use dropdown in header

--- components/layoutAdmin/Header.js
+++ components/layoutAdmin/Header.js
@@ -1,12 +1,8 @@
 // components/Header.js
-import { BsPerson } from "react-icons/bs";
-import { FiBell } from "react-icons/fi";
-// console.log({ sidebar });
-// const sidebar = document.querySelector(".sidebar");
+import { FaRegUserCircle } from "react-icons/fa";
+import CustomDropdown from "../admin/common/CustomDropdown";
 
-// useEffect(() => {
-
-// }, []);
+const options = ["Sultan", "admin@gmail.com", "Logout"];
 const Header = () => {
   const menuHandler = () => {
     const sidebar = document.querySelector(".sidebar");
@@ -16,8 +12,6 @@ const Header = () => {
     } else {
       sidebar.classList.add("collapse");
     }
-
-    //sidebar.classList.remove("collapse");
   };
 
   return (
@@ -41,59 +35,10 @@ const Header = () => {
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ms-auto">
-            <li className="nav-item">
-              <form className="d-flex">
-                <input
-                  className="form-control me-2"
-                  type="search"
-                  placeholder="Search"
-                  aria-label="Search"
-                />
-                <button className="btn btn-outline-secondary" type="submit">
-                  Search
-                </button>
-              </form>
-            </li>
-            <li className="nav-item dropdown">
-              <a
-                className="nav-link dropdown-toggle"
-                href="#"
-                id="navbarDropdown"
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                <FiBell />
-              </a>
-              <ul
-                className="dropdown-menu dropdown-menu-end"
-                aria-labelledby="navbarDropdown"
-              >
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Notification 1
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Notification 2
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Notification 3
-                  </a>
-                </li>
-              </ul>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                <BsPerson className="me-2" />
-                Admin
-              </a>
-            </li>
-          </ul>
+          <div className="navbar-nav ms-auto">
+            <FaRegUserCircle size={24} className="me-2" />
+            <CustomDropdown options={options} />
+          </div>
         </div>
       </div>
     </nav>
