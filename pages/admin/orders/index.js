a9927b280a31fc5bdeeef076ffc0209830bbc770
gazirahad7 <65822873+gazirahad7@users.noreply.github.com>
2023-03-09 13:08:58 +0600

    Add: fetch data from api and re-design table

--- pages/admin/orders/index.js
+++ pages/admin/orders/index.js
@@ -1,28 +1,104 @@
-import CustomTable from "@/components/admin/common/CustomTable";
+import CustomTable, {
+  OrdersTableTH,
+} from "@/components/admin/common/CustomTable";
 import { PageHeader } from "@/components/admin/common/PageHeader";
 import PrivateRoute from "@/components/PrivateRoute";
-import React from "react";
-import { MdAddCircleOutline } from "react-icons/md";
+import axios from "axios";
+import { MdAddCircleOutline, MdOutlineLocalShipping } from "react-icons/md";
 
-const AddButton = ({ name }) => {
-  return (
-    <button className="btn btn-lg fs-5 border  d-flex justify-content-between gap-4 align-items-center px-3 fw-bold cus-bg-primary text-white">
-      {" "}
-      <MdAddCircleOutline size={24} />
-      <span>{name || "Add"}</span>
-    </button>
-  );
-};
+import { useEffect, useState } from "react";
+import { AiOutlineDelete, AiOutlineEye } from "react-icons/ai";
+import { FiEdit } from "react-icons/fi";
+import { dateFormat, trackStatus } from "@/components/admin/common/Fomater";
+import CustomTooltip from "@/components/admin/common/Tooltip";
+import { FaHandHoldingWater } from "react-icons/fa";
 
 function OrdersHomePage() {
+  const [ordersData, setOrdersData] = useState(null);
+
+  console.log({ ordersData });
+
+  const getOrdersData = async () => {
+    const res = await axios.get(
+      "https://crabby-pocketbook-eel.cyclic.app/api/v1/order"
+    );
+    setOrdersData(res.data);
+  };
+
+  useEffect(() => {
+    getOrdersData();
+  }, []);
+
   return (
     <PrivateRoute>
       <PageHeader name="Orders" />
 
-      <div className="card bg-light">
-        <div className="m-3 p-2">
-          <CustomTable tableName="Orders Table" />
-        </div>
+      {/* <CustomTable tableName="Orders Table" headers={OrdersTableTH} /> */}
+
+      <div className="border rounded-3 p-4 cus-table shadow-sm bg-white">
+        <table className="table text-center">
+          <thead>
+            <tr className="fs-6">
+              {OrdersTableTH &&
+                OrdersTableTH.map((header, index) => (
+                  <th scope="col" key={index}>
+                    {header}
+                  </th>
+                ))}
+            </tr>
+          </thead>
+          <tbody className="fs-6 fw-normal">
+            {ordersData &&
+              ordersData.map((row, index) => (
+                <tr key={index}>
+                  <td>{index + 1}</td>
+                  <td>{dateFormat(row?.orderDate)}</td>
+                  <td>{row?.email}</td>
+                  <td>{row?.name}</td>
+                  <td>{row?.phone}</td>
+                  <td>{row?.address}</td>
+                  <td>{row?.products[0]?.name}</td>
+                  <td>{row?.products[0]?.price}</td>
+                  <td>{row?.totalItems}</td>
+                  <td>{row?.totalUniqueItems}</td>
+                  <td>{row?.cartTotal}</td>
+                  <td>
+                    <span
+                      className={`p-2 rounded-3 text-capitalize  text-white ${trackStatus(
+                        row.status
+                      )} `}
+                    >
+                      {row.status}
+                    </span>
+                  </td>
+
+                  <td>
+                    <div className="d-flex justify-content-center gap-2 position-relative">
+                      <span>
+                        <AiOutlineEye size={18} className="text-success" />
+                      </span>
+                      <span>
+                        <MdOutlineLocalShipping
+                          size={18}
+                          className="text-warning"
+                        />
+                      </span>
+                      <span>
+                        <FaHandHoldingWater
+                          size={18}
+                          className="text-primary"
+                        />
+                      </span>
+
+                      <span>
+                        <AiOutlineDelete size={16} className="text-danger" />
+                      </span>
+                    </div>
+                  </td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
       </div>
     </PrivateRoute>
   );
