0a9c0c9054d135943801cae8e6bf4e0046e20789
gazirahad7 <65822873+gazirahad7@users.noreply.github.com>
2023-03-09 13:08:13 +0600

    Update: table headers

--- components/admin/common/CustomTable.js
+++ components/admin/common/CustomTable.js
@@ -45,33 +45,25 @@ export const CarrierTableTH = [
   "Actions",
 ];
 export const BusinessTableTH = ["SL", "Title", "Images", "Body", "Actions"];
+export const OrdersTableTH = [
+  "SL",
+  "Date",
+  "Email",
+  "Name",
+  "Phone",
+  "Address",
+  "P. Name",
+  "Price",
+  "Items",
+  "Unique items",
+  "T. price",
+  "Status",
+  "Actions",
+];
 
 function CustomTable({ tableName, headers, data }) {
   return (
     <>
-      {/* <p className="fs-4 fw-bold">{tableName}</p> */}
-
-      {/* <div classname="card bg-light ">
-          <div classname="my-2 py-2 ">
-            <div className="container my-3">
-              <div className="row ">
-                <div className="col m-0 p-0">
-                  <div className="input-group w-25 ">
-                    <input
-                      type="text"
-                      className="form-control"
-                      placeholder="Search"
-                    />
-                    <button className="btn btn-primary" type="button">
-                     Search
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div> */}
-
       <div className="border rounded-3 p-4 cus-table shadow-sm bg-white">
         <table className="table text-center">
           <thead>
@@ -95,52 +87,7 @@ function CustomTable({ tableName, headers, data }) {
               data.map((row, index) => {
                 <tr key={index}></tr>;
               })}
-            <tr className="my-2  fs-6 fw-normal ">
-              <td className="">1</td>
-              <td>Md Kamal</td>
-              <td>examle@gmail.com</td>
-              <td>
-                <span className="bg-danger p-2 rounded-3 text-white ">
-                  Pending
-                </span>
-              </td>
-              <td>
-                <div className="d-flex justify-content-center gap-2">
-                  <span>
-                    <AiOutlineEye size={18} className="text-success" />
-                  </span>
-                  <span>
-                    <FiEdit size={15} className="text-warning" />
-                  </span>
-                  <span>
-                    <AiOutlineDelete size={16} className="text-danger" />
-                  </span>
-                </div>
-              </td>
-            </tr>
-            <tr className="my-2  fs-6 fw-normal ">
-              <td className="">2</td>
-              <td>Md Kamal</td>
-              <td>examle@gmail.com</td>
-              <td>
-                <span className="bg-warning p-2 rounded-3 text-white ">
-                  Processing
-                </span>
-              </td>
-              <td className="">
-                <div className="d-flex justify-content-center gap-2">
-                  <span>
-                    <AiOutlineEye size={18} className="text-success" />
-                  </span>
-                  <span>
-                    <FiEdit size={15} className="text-warning" />
-                  </span>
-                  <span>
-                    <AiOutlineDelete size={16} className="text-danger" />
-                  </span>
-                </div>
-              </td>
-            </tr>
+
             <tr className="my-2  fs-6 fw-normal ">
               <td className="">2</td>
               <td>Md Kamal</td>
