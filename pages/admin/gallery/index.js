78e3f3f1d3629dab7f1cd27859662e0239293421
gazirahad7 <65822873+gazirahad7@users.noreply.github.com>
2023-03-10 09:47:03 +0600

    Created: gallery table  and  filter by category

--- pages/admin/gallery/index.js
+++ pages/admin/gallery/index.js
@@ -1,6 +1,8 @@
 import { AddButton } from "@/components/admin/common/Buttons";
 import CustomModal from "@/components/admin/common/CustomModal";
-import CustomTable from "@/components/admin/common/CustomTable";
+import CustomTable, {
+  GalleryTableTH,
+} from "@/components/admin/common/CustomTable";
 import { getError } from "@/components/admin/common/error";
 import {
   acceptPattern,
@@ -9,35 +11,34 @@ import {
 import { PageHeader } from "@/components/admin/common/PageHeader";
 import { MyButton } from "@/components/common/Buttons";
 import PrivateRoute from "@/components/PrivateRoute";
+import { useGalleryCollectionQuery } from "@/lib/hook/useApi";
 import axios from "axios";
 import { useState } from "react";
-import { Form } from "react-bootstrap";
+import { Card, Form, Row } from "react-bootstrap";
 import { useForm } from "react-hook-form";
 import { toast } from "react-toastify";
-
- 
- 
- 
-
- 
-
- 
-
+import Skeleton from "react-loading-skeleton";
+import { AiOutlineDelete, AiOutlineEye } from "react-icons/ai";
+import { FiEdit } from "react-icons/fi";
+import { dateFormat } from "@/components/admin/common/Fomater";
 
 const submitHandler = async (data) => {
   console.log({ data });
   const { url, category } = data;
   console.log({ url, category });
   try {
-    await axios.post("https://crabby-pocketbook-eel.cyclic.app/api/v1/gallery", {
-      url, category 
-    });
+    await axios.post(
+      "https://crabby-pocketbook-eel.cyclic.app/api/v1/gallery",
+      {
+        url,
+        category,
+      }
+    );
     toast.success("Gallery successfully added!");
   } catch (err) {
     toast.error(getError(err));
   }
 };
-
 
 const AddGalleryFrom = () => {
   const [url, setImageUrl] = useState("");
@@ -71,13 +72,11 @@ const AddGalleryFrom = () => {
             })}
             onChange={handleInputChange}
           />
-          {errors.url && (
-            <p className="text-danger">{errors.url.message}</p>
-          )}
+          {errors.url && <p className="text-danger">{errors.url.message}</p>}
         </CustomFloatingLabel>
         <p className="fw-bold fs-5">Category</p>
 
-        {['all', 'garden', 'factory', 'office'].map((el) => (
+        {["all", "garden", "factory", "office"].map((el) => (
           <Form.Check
             key={el}
             // label={el}
@@ -108,6 +107,16 @@ const AddGalleryFrom = () => {
 };
 function GalleryHomePage() {
   const [modalShow, setModalShow] = useState(false);
+  const [selectedStatus, setSelectedStatus] = useState("all");
+
+  const { data: gallery, isLoading, isError } = useGalleryCollectionQuery();
+  console.log({ gallery });
+
+  const filteredData =
+    selectedStatus === "all"
+      ? gallery
+      : gallery?.filter((img) => img.category === selectedStatus);
+
   return (
     <PrivateRoute>
       <CustomModal
@@ -128,7 +137,74 @@ function GalleryHomePage() {
           />
         }
       />
-      <CustomTable tableName="Gallery Table" />
+      {/* <CustomTable tableName="Gallery Table" /> */}
+
+      <div className="border rounded-3 p-4 cus-table shadow-sm bg-white">
+        <div className="d-flex justify-content-between my-1">
+          <div>Gallery Table</div>
+          <div>
+            <select
+              className="px-2 py-1 rounded-sm"
+              value={selectedStatus}
+              onChange={(e) => setSelectedStatus(e.target.value)}
+            >
+              <option value="all">All</option>
+              <option value="office">Office</option>
+              <option value="garden">Garden</option>
+            </select>
+          </div>
+        </div>
+        <table className="table text-center">
+          <thead>
+            <tr className="fs-6">
+              {GalleryTableTH &&
+                GalleryTableTH.map((header, index) => (
+                  <th scope="col" key={index}>
+                    {header}
+                  </th>
+                ))}
+            </tr>
+          </thead>
+          <tbody className="fs-6 fw-normal">
+            {isLoading && (
+              <div class="spinner-border text-center" role="status">
+                <span class="visually-hidden">Loading...</span>
+              </div>
+            )}
+
+            {filteredData &&
+              filteredData.map((img, index) => (
+                <tr key={index}>
+                  <td> {index + 1}</td>
+                  <td> {dateFormat(img.createdOn)}</td>
+
+                  <td>{img?.category}</td>
+
+                  <td>
+                    {/* <span className="bg-info p-2 fw-bold text-white rounded-lg">
+                      Open
+                    </span> */}
+                    <img src={img.url} width="60px" />
+                  </td>
+
+                  <td className="">
+                    <div className="d-flex justify-content-center gap-2">
+                      <span>
+                        <AiOutlineEye size={18} className="text-success" />
+                      </span>
+                      <span>
+                        <FiEdit size={15} className="text-warning" />
+                      </span>
+                      <span>
+                        <AiOutlineDelete size={16} className="text-danger" />
+                      </span>
+                    </div>
+                  </td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
+      </div>
     </PrivateRoute>
   );
 }
