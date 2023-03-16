badc35d84c6b0a3865a7dd1d1b90c1308e2dd32d
gazirahad7 <65822873+gazirahad7@users.noreply.github.com>
2023-03-16 15:19:21 +0600

    Add: pagination

--- pages/our-business/index.js
+++ pages/our-business/index.js
@@ -1,15 +1,19 @@
 import React from "react";
- import dynamic from "next/dynamic";
+import dynamic from "next/dynamic";
 import ThreeDotsWave from "@/components/common/ThreeDot";
 import HeroBanner from "@/components/common/Banner";
 
 const OurbusinessShowcase = dynamic(
   () => import("/components/Our-Business/BusinessShowcase.js"),
   {
-    loading: () => <><ThreeDotsWave/></>,
+    loading: () => (
+      <>
+        <ThreeDotsWave />
+      </>
+    ),
   }
 );
-  
+
 // export const getStaticProps = async () => {
 //   const res = await fetch(
 //     "https://arshi365.lamptechs.com/api/admin/todaysDeal"
@@ -19,22 +23,21 @@ const OurbusinessShowcase = dynamic(
 //   revalidate: 3;
 // };
 export const getStaticProps = async () => {
-    const res = await fetch(
-      "https://crabby-pocketbook-eel.cyclic.app/api/v1/business"
-    );
-    const data = await res.json();
-    return {
-      props: { data },
-      revalidate: 10
-    };
+  const res = await fetch(
+    "https://crabby-pocketbook-eel.cyclic.app/api/v1/business"
+  );
+  const data = await res.json();
+  return {
+    props: { data },
+    revalidate: 10,
   };
- 
+};
+
 function OurBusiness({ data }) {
-   
   return (
     <>
- <HeroBanner name='Our Business' /> 
-     
+      <HeroBanner name="Our Business" />
+
       <OurbusinessShowcase data={data} />
     </>
   );
