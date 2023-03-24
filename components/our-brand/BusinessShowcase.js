eecf4032d689e1149b31314edfc537494857adb8
mahmudul165 <mahmudulh883@gmail.com>
2023-03-24 10:27:44 +0600

    added

---
+++ components/our-brand/BusinessShowcase.js
@@ -0,0 +1,301 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+import { motion } from "framer-motion";
+import { Pagination } from "react-bootstrap";
+import { useState } from "react";
+
+// import useAuth from "../../hook/useAuth";
+// import { useCart } from "react-use-cart";
+function BusinessShowcase({ data }) {
+  const ITEMS_PER_PAGE = 9;
+  // const router=useRouter()
+  // console.log(router.pathname)
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const lastItemIndex = currentPage * ITEMS_PER_PAGE;
+  const firstItemIndex = lastItemIndex - ITEMS_PER_PAGE;
+  const currentItems = data.slice(firstItemIndex, lastItemIndex);
+
+  const totalPages = Math.ceil(data.length / ITEMS_PER_PAGE);
+
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
+
+  // const { BuyNow } = useAuth();
+  // const { addItem } = useCart();
+  // Our custom easing
+  let easing = [0.6, -0.05, 0.01, 0.99];
+  //console.log("my data", data);
+  // animate: defines animation
+  // initial: defines initial state of animation or stating point.
+  // exit: defines animation when component exits
+
+  // Custom variant
+  const fadeInUp = {
+    initial: {
+      y: 60,
+      opacity: 0,
+      transition: { duration: 0.6, ease: easing },
+    },
+    animate: {
+      y: 0,
+      opacity: 1,
+      transition: {
+        duration: 0.6,
+        ease: easing,
+      },
+    },
+  };
+
+  const stagger = {
+    animate: {
+      transition: {
+        staggerChildren: 0.1,
+      },
+    },
+  };
+
+  return (
+    <>
+      <motion.div
+        initial="initial"
+        animate="animate"
+        exit={{ opacity: 0 }}
+        className="container my-4 p-3 bg-light"
+        // style={{ backgroundColor: "#ffddde" }}
+      >
+        <motion.div variants={stagger} className="row     my-2  p-2 ">
+          {currentItems ? (
+            currentItems.map((product) => (
+              <div key={product.id} className="col-sm-12 col-md-4  my-2 py-1">
+                <motion.div
+                  variants={fadeInUp}
+                  whileHover={{ scale: 1.05 }}
+                  whileTap={{ scale: 0.95 }}
+                  className="card border-0 "
+                >
+                  {/* {`blogs/${product.title}`} */}
+                  <Link href={`/our-business/${product._id}`} passHref>
+                    {product.image ? (
+                      <motion.img
+                        initial={{ x: 60, opacity: 0 }}
+                        animate={{ x: 0, opacity: 1 }}
+                        transition={{ delay: 0.2 }}
+                        src={product.image}
+                        alt="E-COMMERCE  products"
+                        className="card-img-top  p-2 "
+                        width={336}
+                        height={230}
+                        layout="responsive"
+                      />
+                    ) : (
+                      <motion.img
+                        initial={{ x: 60, opacity: 0 }}
+                        animate={{ x: 0, opacity: 1 }}
+                        transition={{ delay: 0.2 }}
+                        src="https://i.ibb.co/ZT55jTm/Rectangle-1897.png"
+                        alt="E-COMMERCE  products"
+                        className="card-img-top  p-2 "
+                        width={336}
+                        height={230}
+                        layout="responsive"
+                      />
+
+                      // <Skeleton height={200} />
+                    )}
+                  </Link>
+
+                  {/* <Image
+                    src={sunset1}
+                    alt="Sunset"
+                    width={600}
+                    height={450}
+                    layout="responsive"
+                   placeholder="blur"
+                    blurDataURL="data:image/png;base64,[IMAGE_CODE_FROM_PNG_PIXEL]"
+                  /> */}
+
+                  <div className="  p-2 m-2">
+                    <motion.div
+                      animate={{ opacity: 1 }}
+                      initial={{ opacity: 0 }}
+                      className="title"
+                    >
+                      <h3 className="my-1 py-1 fs-5 fw-bold cus-color-primary">
+                        {product.title}
+                      </h3>{" "}
+                    </motion.div>
+                    <p
+                      className="text-gray  py-2  cus-text-justify "
+                      // style={{
+                      //   color: "#FF0099",
+                      //   border: 0,
+                      // }}
+                    >
+                      {product.body?.slice(0, 100)}
+                    </p>
+                  </div>
+                  {/* test button */}
+                  {/* <div className="p-2 m-2 d-flex align-items-center justify-content-center"> */}
+
+                  {/* <button
+                    type="button"
+                    className="btn ml-1 p-1 btn-block btn-sm text-white fs-6 fw-bolder m-1 me-2  "
+                    style={{
+                      backgroundColor: "#59330E",
+                      // color: "#FF0099",
+                      border: 0,
+                    }}
+                    // onClick={() => addItem(product)}
+                  >
+                   Learn More
+                  </button> */}
+                  {/* </div> */}
+                </motion.div>
+              </div>
+            ))
+          ) : (
+            <div className="row    text-center my-2 py-3 ">
+              <div className="col-sm-12 col-md-3 pe-2  ">
+                <div>
+                  <Skeleton height={250}>
+                    <div className="d-flex">
+                      <Skeleton
+                        height={30}
+                        width={90}
+                        borderRadius={10}
+                        highlightColor={"red"}
+                      />
+                      <Skeleton
+                        height={30}
+                        width={90}
+                        borderRadius={10}
+                        highlightColor={"red"}
+                      />
+                    </div>{" "}
+                  </Skeleton>
+                  <div>
+                    <div className="d-flex p-2 justify-content-center align-items-center">
+                      <Skeleton height={30} width={180} />{" "}
+                    </div>
+                    <Skeleton height={30} width={250} />
+                  </div>
+                  <div></div>
+                </div>
+              </div>
+              <div className="col-sm-12 col-md-3 pe-2  ">
+                <div>
+                  <Skeleton height={250}>
+                    <div className="d-flex">
+                      <Skeleton
+                        height={30}
+                        width={90}
+                        borderRadius={10}
+                        highlightColor={"red"}
+                      />
+                      <Skeleton
+                        height={30}
+                        width={90}
+                        borderRadius={10}
+                        highlightColor={"red"}
+                      />
+                    </div>{" "}
+                  </Skeleton>
+                  <div>
+                    <div className="d-flex p-2 justify-content-center align-items-center">
+                      <Skeleton height={30} width={180} />{" "}
+                    </div>
+                    <Skeleton height={30} width={250} />
+                  </div>
+                  <div></div>
+                </div>
+              </div>
+              <div className="col-sm-12 col-md-3 pe-2  ">
+                <div>
+                  <Skeleton height={250}>
+                    <div className="d-flex">
+                      <Skeleton
+                        height={30}
+                        width={90}
+                        borderRadius={10}
+                        highlightColor={"red"}
+                      />
+                      <Skeleton
+                        height={30}
+                        width={90}
+                        borderRadius={10}
+                        highlightColor={"red"}
+                      />
+                    </div>{" "}
+                  </Skeleton>
+                  <div>
+                    <div className="d-flex p-2 justify-content-center align-items-center">
+                      <Skeleton height={30} width={180} />{" "}
+                    </div>
+                    <Skeleton height={30} width={250} />
+                  </div>
+                  <div></div>
+                </div>
+              </div>
+              <div className="col-sm-12 col-md-3 pe-2  ">
+                <div>
+                  <Skeleton height={250}>
+                    <div className="d-flex">
+                      <Skeleton
+                        height={30}
+                        width={90}
+                        borderRadius={10}
+                        highlightColor={"red"}
+                      />
+                      <Skeleton
+                        height={30}
+                        width={90}
+                        borderRadius={10}
+                        highlightColor={"red"}
+                      />
+                    </div>{" "}
+                  </Skeleton>
+                  <div>
+                    <div className="d-flex p-2 justify-content-center align-items-center">
+                      <Skeleton height={30} width={180} />{" "}
+                    </div>
+                    <Skeleton height={30} width={250} />
+                  </div>
+                  <div></div>
+                </div>
+              </div>
+            </div>
+          )}
+        </motion.div>
+      </motion.div>
+
+      <Pagination className="d-flex justify-content-end container my-5">
+        <Pagination.First onClick={() => handlePageChange(1)} />
+        <Pagination.Prev
+          onClick={() => handlePageChange(currentPage - 1)}
+          disabled={currentPage === 1}
+        />
+        {[...Array(totalPages)].map((_, index) => (
+          <Pagination.Item
+            key={index}
+            active={currentPage === index + 1}
+            onClick={() => handlePageChange(index + 1)}
+          >
+            {index + 1}
+          </Pagination.Item>
+        ))}
+        <Pagination.Next
+          onClick={() => handlePageChange(currentPage + 1)}
+          disabled={currentPage === totalPages}
+        />
+        <Pagination.Last onClick={() => handlePageChange(totalPages)} />
+      </Pagination>
+    </>
+  );
+}
+
+export default BusinessShowcase;
---
+++ pages/our-brand/[slugId].js
@@ -0,0 +1,186 @@
+import React, { useEffect, useState } from "react";
+import { Container, Image, Card } from "react-bootstrap";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+import { motion } from "framer-motion";
+
+import SliderImg from "public/slider-2.jpg";
+
+import HeroBanner from "@/components/common/Banner";
+ 
+export const getStaticPaths = async () => {
+   
+ 
+  const res = await fetch(
+    "https://crabby-pocketbook-eel.cyclic.app/api/v1/business"
+  );
+  const data = await res.json();
+  const paths = await data.map((post) => {
+    return {
+      params: { slugId: post?._id.toString() },
+    };
+  });
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async ({ params }) => {
+  const res = await fetch(
+    `https://crabby-pocketbook-eel.cyclic.app/api/v1/business/${params.slugId}`
+  );
+  console.log("url:", res);
+  const data = await res.json();
+  console.log("product data is :", data);
+
+  return {
+    props: { data },
+    revalidate: 10,
+  };
+};
+
+function BlogDetails({ data }) {
+  return (
+    <div className="   ">
+      <HeroBanner name="Our Business" />
+      {data ? (
+        <Container className="container my-4">
+          <Card className="border-0  p-2">
+            {/* <Image
+  src="/image.jpg"
+  alt="description"
+  width={500}
+  height={500}
+/> */}
+            <Card.Img
+              className="h-50"
+              variant="top"
+              // src="https://i.ibb.co/7C7M7QX/tea-5.jpg"
+              src={data.image}
+            />
+            <p className="cus-color-primary my-3 fs-4  ">{data.title}</p>
+
+            <p className="  text-justify cus-text-justify ">{data.body}</p>
+          </Card>
+        </Container>
+      ) : (
+        <div className="row    text-center my-2 py-3 ">
+          <div className="col-sm-12 col-md-3 pe-2  ">
+            <div>
+              <Skeleton height={250}>
+                <div className="d-flex">
+                  <Skeleton
+                    height={30}
+                    width={90}
+                    borderRadius={10}
+                    highlightColor={"red"}
+                  />
+                  <Skeleton
+                    height={30}
+                    width={90}
+                    borderRadius={10}
+                    highlightColor={"red"}
+                  />
+                </div>{" "}
+              </Skeleton>
+              <div>
+                <div className="d-flex p-2 justify-content-center align-items-center">
+                  <Skeleton height={30} width={180} />{" "}
+                </div>
+                <Skeleton height={30} width={250} />
+              </div>
+              <div></div>
+            </div>
+          </div>
+          <div className="col-sm-12 col-md-3 pe-2  ">
+            <div>
+              <Skeleton height={250}>
+                <div className="d-flex">
+                  <Skeleton
+                    height={30}
+                    width={90}
+                    borderRadius={10}
+                    highlightColor={"red"}
+                  />
+                  <Skeleton
+                    height={30}
+                    width={90}
+                    borderRadius={10}
+                    highlightColor={"red"}
+                  />
+                </div>{" "}
+              </Skeleton>
+              <div>
+                <div className="d-flex p-2 justify-content-center align-items-center">
+                  <Skeleton height={30} width={180} />{" "}
+                </div>
+                <Skeleton height={30} width={250} />
+              </div>
+              <div></div>
+            </div>
+          </div>
+          <div className="col-sm-12 col-md-3 pe-2  ">
+            <div>
+              <Skeleton height={250}>
+                <div className="d-flex">
+                  <Skeleton
+                    height={30}
+                    width={90}
+                    borderRadius={10}
+                    highlightColor={"red"}
+                  />
+                  <Skeleton
+                    height={30}
+                    width={90}
+                    borderRadius={10}
+                    highlightColor={"red"}
+                  />
+                </div>{" "}
+              </Skeleton>
+              <div>
+                <div className="d-flex p-2 justify-content-center align-items-center">
+                  <Skeleton height={30} width={180} />{" "}
+                </div>
+                <Skeleton height={30} width={250} />
+              </div>
+              <div></div>
+            </div>
+          </div>
+          <div className="col-sm-12 col-md-3 pe-2  ">
+            <div className="container">
+              <Skeleton height={1000}>
+                <div>
+                  <Skeleton
+                    height={400}
+                    width={800}
+                    borderRadius={10}
+                    highlightColor={"red"}
+                  />
+                </div>{" "}
+                <div>
+                  <Skeleton
+                    height={200}
+                    width={800}
+                    borderRadius={10}
+                    highlightColor={"red"}
+                  />
+                </div>{" "}
+                <div>
+                  <Skeleton
+                    height={400}
+                    width={800}
+                    borderRadius={10}
+                    highlightColor={"red"}
+                  />
+                </div>{" "}
+              </Skeleton>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default BlogDetails;
