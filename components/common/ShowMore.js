1f73248318a3df2f8bb5807e43ddfd1482f264ed
gazirahad7 <65822873+gazirahad7@users.noreply.github.com>
2023-04-12 13:37:07 +0600

    Added: new component for view description

---
+++ components/common/ShowMore.js
@@ -0,0 +1,26 @@
+import { Modal } from "react-bootstrap";
+
+const ShowMore = (props) => {
+  const { title, subTitle, description } = props;
+
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">{title}</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <p className="fw-bold fs-4 my-2">{subTitle}</p>
+        <p className="my-3 py-1 px-2   text-secondary text-sm text-justify">
+          {description}
+        </p>
+      </Modal.Body>
+    </Modal>
+  );
+};
+
+export default ShowMore;
