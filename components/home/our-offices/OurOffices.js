aaa7ffa6225e2c3782b359cff18d09b2f35771b9
gazirahad7 <65822873+gazirahad7@users.noreply.github.com>
2023-04-13 16:07:18 +0600

    Update

--- components/home/our-offices/OurOffices.js
+++ components/home/our-offices/OurOffices.js
@@ -1,10 +1,6 @@
 import SectionTitle from "@/components/common/SectionTitle";
- 
-import {
- OFFICES_ENDPOINT,
-  
-  useOfficesCollectionQuery,
-} from "@/lib/hook/useApi";
+
+import { OFFICES_ENDPOINT, useOfficesCollectionQuery } from "@/lib/hook/useApi";
 import React from "react";
 import { Row, Col, Card, Image } from "react-bootstrap";
 import {
@@ -14,44 +10,46 @@ import {
   FaPhoneAlt,
 } from "react-icons/fa";
 function OurOffices() {
-   
   const { data: ourOffices, isLoading, isError } = useOfficesCollectionQuery();
   return (
     <div className="container my-4">
-      <Row className="align-items-center justify-content-between">
+      <Row className="justify-content-between">
         <SectionTitle title="Our Offices " />
         {ourOffices?.map((product) => (
           <Col sm={12} md={4} key={product._id}>
             {/* <Card style={{ width: "22rem", height: "auto"}}> */}
-            <Card className="mx-auto my-3" >
-              <Image
-                src={product.image}
-                alt="text22"
-                fluid
-               // className="w-100 h-auto object-fit-cover"
-              />
-            </Card>
-            <Card.Body className="py-2">
-              <Card.Title>{product.name}</Card.Title>
-              <Card.Text className="py-2 mb-1 cus-color-secondary">
-                <span>
-                  <FaMapMarkerAlt className="d-inline me-2" />
-                </span>{" "}
-                {product.location}
-              </Card.Text>
-              <Card.Text className="cus-color-secondary">
-                <span className="py-2 my-2 ">
-                  <FaPhoneAlt className="d-inline me-1 " />{" "}
-                </span>
-                {product.phone}
-              </Card.Text>
-              <Card.Text className="py-2 my-2 cus-color-secondary">
-                <span>
-                  <FaEnvelope className="d-inline me-2" />
-                </span>
-                {product.email}
-              </Card.Text>
-            </Card.Body>
+            <div className=" my-3 ">
+              <div>
+                <Image
+                  src={product.image}
+                  alt="Office"
+                  fluid
+                  className="w-100 h-auto object-fit-cover border rounded"
+                />
+              </div>
+              <div className="py-2 ">
+                <p className="fs-5  fw-bold my-2">{product.name}</p>
+                <div className="py-2  mb-1 cus-color-secondary fs-6 cus-fw-4">
+                  <span>
+                    <FaMapMarkerAlt className="d-inline me-2" />
+                  </span>{" "}
+                  {product.location}
+                </div>
+                <div className="cus-color-secondary fs-6 cus-fw-4">
+                  <span className="py-2 my-2 ">
+                    <FaPhoneAlt className="d-inline me-1 " />{" "}
+                  </span>
+                  {product.phone}
+                </div>
+                <div className="py-2 my-2 cus-color-secondary fs-6 cus-fw-4">
+                  <span>
+                    <FaEnvelope className="d-inline me-2" />
+                  </span>
+                  {product.email}
+                </div>
+              </div>
+            </div>
+
             {/* </Card> */}
           </Col>
         ))}
